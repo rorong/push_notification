df7d65d6c996ae5dff2012ee22c64690c622a8e8
rorong <rordev@ongraph.com>
2019-05-31 15:33:03 +0530

    first commit

--- app/controllers/push_notifications_controller.rb
+++ app/controllers/push_notifications_controller.rb
@@ -7,7 +7,8 @@ class PushNotificationsController < ApplicationController
     @push_notification = PushNotification.new(push_notification_params)
     #device_token stored with mobile app
     #platform either android or iOS this is required due to multiple devices.
-    SendMessageWorker.perform_async(platform, device_token, @push_notification.message)
+    device_token = 'c968jE-fr8U:APA91bHwJ1oioiXq0WNRCLiRvjIzhVVM4TKLljBBc1kJs7rGenR0kPOw3XCKlZ4kUOqwASjomZRYSWjlr2v25rgB7yTPYBImnsAlCJ2L6A0sv-mqn9zQ0CO5aLCge7mvY2gw9Qe1kIZg'
+    SendMessageWorker.perform_async('android', device_token, @push_notification.message)
     flash[:notice] = "Your message was successfully scheduled and is currently being delivered to its recipients."
     redirect_to root_path
   end
